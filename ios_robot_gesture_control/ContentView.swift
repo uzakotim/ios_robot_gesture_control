c1cb3bb67bdbb33e15ff20cb06c80af3db5cfd7f
uzakotim <timauzakov@gmail.com>
2026-02-14 12:40:35 +0500

    Initial Commit

---
+++ ios_robot_gesture_control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios_robot_gesture_control
+//
+//  Created by Timur Uzakov on 14/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios_robot_gesture_control/ios_robot_gesture_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_robot_gesture_controlApp.swift
+//  ios_robot_gesture_control
+//
+//  Created by Timur Uzakov on 14/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_robot_gesture_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
