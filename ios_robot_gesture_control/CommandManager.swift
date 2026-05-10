049d495bd9ac06fc80c574d2c0ffc07e1ad3ee35
uzakotim <timauzakov@gmail.com>
2026-05-10 14:53:56 +0500

    added small pauses between commands

--- ios_robot_gesture_control/CommandManager.swift
+++ ios_robot_gesture_control/CommandManager.swift
@@ -32,44 +32,56 @@ class CommandManager: ObservableObject {
 
     let commandSequences: [String: [SequenceAction]] = [
         "happiness": [
-            .command("q 200"), .command("q 200"), .pause(0.2), // Quick wiggle
-            .command("e 200"), .command("e 200"), .pause(0.2),
-            .command("q 200"), .command("q 200"), .pause(0.2),
-            .command("e 200"), .command("e 200"), .pause(0.2),
-            .command("w 180"), .command("w 180"), .pause(0.4), // Happy jump forward
+            .command("q 200"),.pause(0.01), .command("q 200"),.pause(0.1),.pause(0.1), // Quick wiggle
+            .command("e 200"),.pause(0.01), .command("e 200"),.pause(0.1),.pause(0.1),
+            .command("q 200"),.pause(0.01), .command("q 200"),.pause(0.1),.pause(0.1),
+            .command("e 200"),.pause(0.01), .command("e 200"),.pause(0.1),.pause(0.1),
+            .command("w 180"),.pause(0.01), .command("w 180"),.pause(0.1),.pause(0.1), // Happy jump forward
+            .command("k 0"),
+            .command("k 0"),
             .command("k 0")
         ],
         "sadness": [
-            .command("s 200"), .command("s 200"),.pause(0.2),// Slow slump back
+            .command("s 200"),.pause(0.01), .command("s 200"),.pause(0.2),// Slow slump back
             .command("q 150"), .pause(1.2), // Slow look down/side
+            .command("k 0"),
+            .command("k 0"),
             .command("k 0")
         ],
         "anger": [
-            .command("w 200"), .command("w 200"), .pause(0.1), // Aggressive vibration
-            .command("s 200"), .command("s 200"), .pause(0.1),
-            .command("w 200"), .command("w 200"), .pause(0.1),
-            .command("s 200"), .command("s 200"), .pause(0.1),
-            .command("q 200"), .command("q 200"), .pause(0.5), // Aggressive turn
-            .command("e 200"), .command("e 200"), .pause(0.5),
+            .command("w 200"),.pause(0.01), .command("w 200"), .pause(0.1), // Aggressive vibration
+            .command("s 200"),.pause(0.01), .command("s 200"), .pause(0.1),
+            .command("w 200"),.pause(0.01), .command("w 200"), .pause(0.1),
+            .command("s 200"),.pause(0.01), .command("s 200"), .pause(0.1),
+            .command("q 200"),.pause(0.01), .command("q 200"), .pause(0.5), // Aggressive turn
+            .command("e 200"),.pause(0.01), .command("e 200"), .pause(0.5),
+            .command("k 0"),
+            .command("k 0"),
             .command("k 0")
         ],
         "fear": [
-            .command("s 200"), .command("s 200"), .pause(0.5), // Rapid retreat
-            .command("q 180"), .command("q 180"), .pause(0.2), // Quick look around
-            .command("e 180"), .command("e 180"), .pause(0.2),
-            .command("s 200"), .command("s 200"), .pause(0.3), // More retreat
+            .command("s 200"),.pause(0.01), .command("s 200"), .pause(0.5), // Rapid retreat
+            .command("q 180"),.pause(0.01), .command("q 180"), .pause(0.2), // Quick look around
+            .command("e 180"),.pause(0.01), .command("e 180"), .pause(0.2),
+            .command("s 200"),.pause(0.01), .command("s 200"), .pause(0.3), // More retreat
+            .command("k 0"),
+            .command("k 0"),
             .command("k 0")
         ],
         "disgust": [
-            .command("s 160"), .command("s 160"), .pause(0.4), // Backing away slowly
-            .command("e 190"), .command("e 190"), .pause(0.6), // Sharp "turning away"
+            .command("s 160"),.pause(0.01), .command("s 160"), .pause(0.4), // Backing away slowly
+            .command("e 190"),.pause(0.01), .command("e 190"), .pause(0.6), // Sharp "turning away"
+            .command("k 0"),
+            .command("k 0"),
             .command("k 0")
         ],
         "surprise": [
-            .command("s 200"), .command("s 200"), .pause(0.2), // Sudden jump back
-            .command("stop"), .pause(0.5),  // Freeze
-            .command("q 170"), .command("q 170"), .pause(0.3), // Confused look
-            .command("e 170"), .command("e 170"), .pause(0.3),
+            .command("s 200"),.pause(0.01), .command("s 200"), .pause(0.2), // Sudden jump back
+            .command("k 0"),.pause(0.01),.command("k 0"),      .pause(0.5),  // Freeze
+            .command("q 170"),.pause(0.01), .command("q 170"), .pause(0.3), // Confused look
+            .command("e 170"),.pause(0.01), .command("e 170"), .pause(0.3),
+            .command("k 0"),
+            .command("k 0"),
             .command("k 0")
         ]
     ]
@@ -141,6 +153,7 @@ class CommandManager: ObservableObject {
         sequenceTask?.cancel()
         isPlayingSequence = false
         sequenceTask = nil
+        sendCommand("k 0", force: true)
         sendCommand("k 0", force: true)
     }
 
